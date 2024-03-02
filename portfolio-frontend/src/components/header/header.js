e34e28c1084b5a75f4be83be935430fa4ab974f8
Ywintersss <shawnpvp@gmail.com>
2024-03-02 22:54:22 +0800

    Hello World

---
+++ portfolio-frontend/src/components/header/header.js
@@ -0,0 +1,12 @@
+import './header.css'
+import React from "react";
+
+function header(){
+    return(
+        <header>
+            <div className='hello-world'>Hello World</div>
+        </header>
+    )
+}
+
+export default header
